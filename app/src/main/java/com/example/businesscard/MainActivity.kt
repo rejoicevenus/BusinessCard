947b7e46789dc6eeb945f42402ac198bebc9b68e
Venus Munyi <rejoicevenus@gmail.com>
2025-03-13 12:49:42 +0300

    changed the font size for the second text

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -86,8 +86,8 @@ fun BusinessCard() {
 
             Text(
                 text = "Android Developer Extraordinaire",
-                fontSize = 14.sp,
-                fontWeight = FontWeight.SemiBold,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
                 color = Color(0xFF4F772D) // Dark Green
             )
 
@@ -123,7 +123,7 @@ fun ContactInfo(icon: ImageVector, text: String) {
             modifier = Modifier.size(24.dp)
         )
         Spacer(modifier = Modifier.width(16.dp))
-        Text(text = text, textAlign = TextAlign.Center, fontSize = 14.sp)
+        Text(text = text, textAlign = TextAlign.Justify, fontSize = 14.sp)
     }
 }
 
